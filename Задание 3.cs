733f1cec71c1c5fae167218b549bc407aedf7d97
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-03-01 20:21:05 +0300

    Add files via upload

---
+++ Задание 3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seasons season = Seasons.None;
+            switch (number)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    season = seasons.winter;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = seasons.Spring;
+                    break;
+                case 6:
+                case 7:sons.Autu
+                case 8:
+                    season = Seasons.Summer;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = Seasons.autumn;
+                    break;
+
+            }
+            return season;
+        }
+    }
+}
