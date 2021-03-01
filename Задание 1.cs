5cb57253c2bfc52497e2a03cd28374162724e812
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-03-01 20:20:20 +0300

    Add files via upload

---
+++ Задание 1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string GetFullName(string firstName, string lastName, string pastronymic)
+            {
+                return $"{lastName} {firstName} {patronynic}";
+            }
+        }
+    }
+}
